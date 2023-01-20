1fd52e126bf257ca3e1e70a0eaf5c81fbb8b640d
ramgoplk <ramgopalkoya@gmail.com>
2023-01-20 08:00:46 +0530

    up

--- loadbalancers.tf
+++ loadbalancers.tf
@@ -1,149 +1,149 @@
 
-# # resource "azurerm_public_ip" "publicip1" {
-# #   name                = var.publicip.name
-# #   location            = var.location
-# #   resource_group_name = var.resourcegroup.name
-# #   allocation_method   = "Static"
-# # }
-
-# resource "azurerm_lb" "loadbalancer1" {
-#   name                = var.loadbalancer1.name
+# resource "azurerm_public_ip" "publicip1" {
+#   name                = var.publicip.name
 #   location            = var.location
 #   resource_group_name = var.resourcegroup.name
+#   allocation_method   = "Static"
+# }
 
-#   frontend_ip_configuration {
-#     name                          = "PublicIPAddress"
-#     public_ip_address_id          = azurerm_public_ip.public_ip.id
-#     subnet_id                     = var.frontend_subnet_id
-#     # private_ip_address            = var.frontend_private_ip_address
-#     # private_ip_address_allocation = var.frontend_private_ip_address_allocation
+resource "azurerm_lb" "loadbalancer1" {
+  name                = var.loadbalancer1.name
+  location            = var.location
+  resource_group_name = var.resourcegroup.name
+
+  frontend_ip_configuration {
+    name                          = "PublicIPAddress"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+    subnet_id                     = var.frontend_subnet_id
+    # private_ip_address            = var.frontend_private_ip_address
+    # private_ip_address_allocation = var.frontend_private_ip_address_allocation
+  }
+}
+
+
+resource "azurerm_lb_backend_address_pool" "example" {
+  loadbalancer_id = azurerm_lb.loadbalancer1.id
+  name            = "BackEndAddressPool"
+}
+
+
+resource "azurerm_lb_probe" "example" {
+  loadbalancer_id = azurerm_lb.loadbalancer1.id
+  name            = "ssh-running-probe"
+  port            = 22
+}
+
+resource "azurerm_lb_rule" "example" {
+  loadbalancer_id                = azurerm_lb.loadbalancer1.id
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 3389
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_lb_nat_rule" "example1" {
+  resource_group_name            = var.resourcegroup.name
+  loadbalancer_id                = azurerm_lb.loadbalancer1.id
+  name                           = "RDPAccess"
+  protocol                       = "Tcp"
+  frontend_port_start            = 3000
+  frontend_port_end              = 3389
+  backend_port                   = 3389
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.example.id
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+
+
+//load balancer2 "external load balancer"  "LB2"
+
+# resource "azurerm_public_ip" "publicip2" {
+#   name                = "PublicIPForLB"
+#   location            = var.location
+#   resource_group_name = var.resourcegroup.name
+#   allocation_method   = "Static"
+# }
+
+resource "azurerm_lb" "loadbalancer2" {
+  name                = var.loadbalancer2.name
+  location            = var.location
+  resource_group_name = var.resourcegroup.name
+
+  frontend_ip_configuration {
+    name                          = "PublicIPAddress"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+    subnet_id                     = var.frontend_subnet_id
+    private_ip_address            = var.frontend_private_ip_address
+    private_ip_address_allocation = var.frontend_private_ip_address_allocation
+  }
+}
+
+
+resource "azurerm_lb_backend_address_pool" "example" {
+  loadbalancer_id = azurerm_lb.loadbalancer2.id
+  name            = "BackEndAddressPool"
+}
+
+
+resource "azurerm_lb_probe" "example" {
+  loadbalancer_id = azurerm_lb.loadbalancer2.id
+  name            = "ssh-running-probe"
+  port            = 22
+}
+
+resource "azurerm_lb_rule" "example" {
+  loadbalancer_id                = azurerm_lb.loadbalancer2.id
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 3389
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_lb_nat_rule" "example1" {
+  resource_group_name            = var.resourcegroup.name
+  loadbalancer_id                = azurerm_lb.loadbalancer2.id
+  name                           = "RDPAccess"
+  protocol                       = "Tcp"
+  frontend_port_start            = 3000
+  frontend_port_end              = 3389
+  backend_port                   = 3389
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.example.id
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+
+
+
+# //public ip
+
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-resources"
+#   location = "West Europe"
+# }
+
+# resource "azurerm_public_ip" "example" {
+#   name                = "acceptanceTestPublicIp1"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   allocation_method   = "Static"
+
+#   tags = {
+#     environment = "Production"
 #   }
 # }
 
+# resource "azurerm_public_ip_prefix" "example" {
+#   name                = "acceptanceTestPublicIpPrefix1"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
 
-# resource "azurerm_lb_backend_address_pool" "example" {
-#   loadbalancer_id = azurerm_lb.loadbalancer1.id
-#   name            = "BackEndAddressPool"
-# }
+#   prefix_length = 31
 
-
-# resource "azurerm_lb_probe" "example" {
-#   loadbalancer_id = azurerm_lb.loadbalancer1.id
-#   name            = "ssh-running-probe"
-#   port            = 22
-# }
-
-# resource "azurerm_lb_rule" "example" {
-#   loadbalancer_id                = azurerm_lb.loadbalancer1.id
-#   name                           = "LBRule"
-#   protocol                       = "Tcp"
-#   frontend_port                  = 3389
-#   backend_port                   = 3389
-#   frontend_ip_configuration_name = "PublicIPAddress"
-# }
-
-# resource "azurerm_lb_nat_rule" "example1" {
-#   resource_group_name            = var.resourcegroup.name
-#   loadbalancer_id                = azurerm_lb.loadbalancer1.id
-#   name                           = "RDPAccess"
-#   protocol                       = "Tcp"
-#   frontend_port_start            = 3000
-#   frontend_port_end              = 3389
-#   backend_port                   = 3389
-#   backend_address_pool_id        = azurerm_lb_backend_address_pool.example.id
-#   frontend_ip_configuration_name = "PublicIPAddress"
-# }
-
-
-
-# //load balancer2 "external load balancer"  "LB2"
-
-# # resource "azurerm_public_ip" "publicip2" {
-# #   name                = "PublicIPForLB"
-# #   location            = var.location
-# #   resource_group_name = var.resourcegroup.name
-# #   allocation_method   = "Static"
-# # }
-
-# resource "azurerm_lb" "loadbalancer2" {
-#   name                = var.loadbalancer2.name
-#   location            = var.location
-#   resource_group_name = var.resourcegroup.name
-
-#   frontend_ip_configuration {
-#     name                          = "PublicIPAddress"
-#     public_ip_address_id          = azurerm_public_ip.public_ip.id
-#     subnet_id                     = var.frontend_subnet_id
-#     private_ip_address            = var.frontend_private_ip_address
-#     private_ip_address_allocation = var.frontend_private_ip_address_allocation
+#   tags = {
+#     environment = "Production"
 #   }
 # }
-
-
-# resource "azurerm_lb_backend_address_pool" "example" {
-#   loadbalancer_id = azurerm_lb.loadbalancer2.id
-#   name            = "BackEndAddressPool"
-# }
-
-
-# resource "azurerm_lb_probe" "example" {
-#   loadbalancer_id = azurerm_lb.loadbalancer2.id
-#   name            = "ssh-running-probe"
-#   port            = 22
-# }
-
-# resource "azurerm_lb_rule" "example" {
-#   loadbalancer_id                = azurerm_lb.loadbalancer2.id
-#   name                           = "LBRule"
-#   protocol                       = "Tcp"
-#   frontend_port                  = 3389
-#   backend_port                   = 3389
-#   frontend_ip_configuration_name = "PublicIPAddress"
-# }
-
-# resource "azurerm_lb_nat_rule" "example1" {
-#   resource_group_name            = var.resourcegroup.name
-#   loadbalancer_id                = azurerm_lb.loadbalancer2.id
-#   name                           = "RDPAccess"
-#   protocol                       = "Tcp"
-#   frontend_port_start            = 3000
-#   frontend_port_end              = 3389
-#   backend_port                   = 3389
-#   backend_address_pool_id        = azurerm_lb_backend_address_pool.example.id
-#   frontend_ip_configuration_name = "PublicIPAddress"
-# }
-
-
-
-
-# # //public ip
-
-# # resource "azurerm_resource_group" "example" {
-# #   name     = "example-resources"
-# #   location = "West Europe"
-# # }
-
-# # resource "azurerm_public_ip" "example" {
-# #   name                = "acceptanceTestPublicIp1"
-# #   resource_group_name = azurerm_resource_group.example.name
-# #   location            = azurerm_resource_group.example.location
-# #   allocation_method   = "Static"
-
-# #   tags = {
-# #     environment = "Production"
-# #   }
-# # }
-
-# # resource "azurerm_public_ip_prefix" "example" {
-# #   name                = "acceptanceTestPublicIpPrefix1"
-# #   location            = azurerm_resource_group.example.location
-# #   resource_group_name = azurerm_resource_group.example.name
-
-# #   prefix_length = 31
-
-# #   tags = {
-# #     environment = "Production"
-# #   }
-# # }
 
 
--- variable.tf
+++ variable.tf
@@ -12,177 +12,177 @@ variable "tenant_id" {
   type = string
 }
 
-# variable "fgtvm_name" {
+variable "fgtvm_name" {
+  type    = string
+  default = "fortgate vm"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_F4"
+}
+
+# // To use custom image
+# // by default is false
+# variable "custom" {
+#   default = false
+# }
+
+# //  Custom image blob uri
+# variable "customuri" {
 #   type    = string
-#   default = "fortgate vm"
+#   default = "<custom image blob uri>"
 # }
 
-# variable "vm_size" {
+# variable "custom_image_name" {
 #   type    = string
-#   default = "Standard_F4"
+#   default = "<custom image name>"
 # }
 
-# # // To use custom image
-# # // by default is false
-# # variable "custom" {
-# #   default = false
-# # }
-
-# # //  Custom image blob uri
-# # variable "customuri" {
-# #   type    = string
-# #   default = "<custom image blob uri>"
-# # }
-
-# # variable "custom_image_name" {
-# #   type    = string
-# #   default = "<custom image name>"
-# # }
-
-# # variable "custom_image_resource_group_name" {
-# #   type    = string
-# #   default = "<custom image resource group>"
-# # }
-
-# // License Type to create FortiGate-VM
-# // Provide the license type for FortiGate-VM Instances, either byol or payg.
-# variable "license_type" {
-#   default = "payg"
-# }
-
-# variable "publisher" {
+# variable "custom_image_resource_group_name" {
 #   type    = string
-#   default = "fortinet"
+#   default = "<custom image resource group>"
 # }
 
-# variable "fgtoffer" {
-#   type    = string
-#   default = "fortinet_fortigate-vm_v5"
-# }
+// License Type to create FortiGate-VM
+// Provide the license type for FortiGate-VM Instances, either byol or payg.
+variable "license_type" {
+  default = "payg"
+}
 
-# // BYOL sku: fortinet_fg-vm
-# // PAYG sku: fortinet_fg-vm_payg_20190624
-# variable "fgtsku" {
-#   type = map(any)
-#   default = {
-#     byol = "fortinet_fg-vm"
-#     payg = "fortinet_fg-vm_payg_2022"
-#   }
-# }
+variable "publisher" {
+  type    = string
+  default = "fortinet"
+}
 
-# variable "fgtversion" {
-#   type    = string
-#   default = "7.2.2"
-# }
+variable "fgtoffer" {
+  type    = string
+  default = "fortinet_fortigate-vm_v5"
+}
 
-# variable "adminusername" {
-#   type    = string
-#   default = "azureadmin"
-# }
+// BYOL sku: fortinet_fg-vm
+// PAYG sku: fortinet_fg-vm_payg_20190624
+variable "fgtsku" {
+  type = map(any)
+  default = {
+    byol = "fortinet_fg-vm"
+    payg = "fortinet_fg-vm_payg_2022"
+  }
+}
 
-# variable "adminpassword" {
-#   type    = string
-#   default = "Fortinet@123#"
-# }
+variable "fgtversion" {
+  type    = string
+  default = "7.2.2"
+}
 
-# variable "location" {
-#   type    = string
-#   default = "westus2"
-# }
+variable "adminusername" {
+  type    = string
+  default = "azureadmin"
+}
 
-# variable "vnetcidr" {
-#   default = "10.1.0.0/16"
-# }
+variable "adminpassword" {
+  type    = string
+  default = "Fortinet@123#"
+}
 
-# variable "publiccidr" {
-#   default = "10.1.0.0/24"
-# }
+variable "location" {
+  type    = string
+  default = "westus2"
+}
 
-# variable "privatecidr" {
-#   default = "10.1.1.0/24"
-# }
+variable "vnetcidr" {
+  default = "10.1.0.0/16"
+}
 
-# variable "bootstrap-fgtvm" {
-#   // Change to your own path
-#   type    = string
-#   default = "fgtvm.conf"
-# }
+variable "publiccidr" {
+  default = "10.1.0.0/24"
+}
 
-# // license file for the fgt
-# variable "license" {
-#   // Change to your own byol license file, license.lic
-#   type    = string
-#   default = "license.txt"
-# }
-# //vnet
-# variable "vnet" {
-#   type = string
-# }
+variable "privatecidr" {
+  default = "10.1.1.0/24"
+}
+
+variable "bootstrap-fgtvm" {
+  // Change to your own path
+  type    = string
+  default = "fgtvm.conf"
+}
+
+// license file for the fgt
+variable "license" {
+  // Change to your own byol license file, license.lic
+  type    = string
+  default = "license.txt"
+}
+//vnet
+variable "vnet" {
+  type = string
+}
 //resource group
 variable "resourcegroup" {
   type = string
 }
-# //vnet
-# variable "vnet" {
-#   type = string
-# }
+//vnet
+variable "vnet" {
+  type = string
+}
 
-# variable "keyvault" {
-#   type    = string
-#   default = "fgtkeyvault"
-# }
+variable "keyvault" {
+  type    = string
+  default = "fgtkeyvault"
+}
 
-# variable "public_ip" {
-#   type    = string
-#   default = "fgtfwPublicIP"
-# }
+variable "public_ip" {
+  type    = string
+  default = "fgtfwPublicIP"
+}
 
-# variable "loadbalancer2" {
-#   type    = string
-#   default = "internal loadbalancer"
-# }
+variable "loadbalancer2" {
+  type    = string
+  default = "internal loadbalancer"
+}
 
-# variable "loadbalancer1" {
-#   type    = string
-#   default = "external loadbalancer"
-# }
+variable "loadbalancer1" {
+  type    = string
+  default = "external loadbalancer"
+}
 
-# variable "subnet1" {
-#   type    = string
-#   default = "publicsubnet"
-# }
+variable "subnet1" {
+  type    = string
+  default = "publicsubnet"
+}
 
-# variable "subnet2" {
-#   type    = string
-#   default = "privatesubnet"
-# }
+variable "subnet2" {
+  type    = string
+  default = "privatesubnet"
+}
 
-# variable "subnet3" {
-#   type    = string
-#   default = "protected subnet"
-# }
+variable "subnet3" {
+  type    = string
+  default = "protected subnet"
+}
 
-# variable "publiccidr" {
-#   type    = string
-#   default = ""
-# }
+variable "publiccidr" {
+  type    = string
+  default = ""
+}
 
-# variable "privatecidr" {
-#   type    = string
-#   default = ""
-# }
+variable "privatecidr" {
+  type    = string
+  default = ""
+}
 
-# variable "protectedcidr" {
-#   type    = string
-#   default = ""
-# }
+variable "protectedcidr" {
+  type    = string
+  default = ""
+}
 
-# variable "routetable" {
-#   type    = string
-#   default = "InternalRouteTable1"
-# }
+variable "routetable" {
+  type    = string
+  default = "InternalRouteTable1"
+}
 
-# variable "storageaccount" {
-#   type    = string
-#   default = "fgtstorageaccount"
-# }
+variable "storageaccount" {
+  type    = string
+  default = "fgtstorageaccount"
+}
